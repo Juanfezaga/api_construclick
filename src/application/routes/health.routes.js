449207f8c04e39666f0237ab8d716e4523bbb620
johnny.pacheco1 <johnny.pacheco@dafiti.com.co>
2021-08-07 21:53:18 -0500

    health with name

--- src/application/routes/health.routes.js
+++ src/application/routes/health.routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
   res.send({
-    health: 'Ok',
+    health: 'Ok - Construclick',
     date: new Date().toUTCString(),
   });
 });
