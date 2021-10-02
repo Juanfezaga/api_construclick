9c02b95f6c9cf245e2a2eb1d6f7eb44691e72de8
johnny.pacheco1 <johnny.pacheco@dafiti.com.co>
2021-10-02 15:12:53 -0500

    Category api tested

--- src/application/routes/index.js
+++ src/application/routes/index.js
@@ -4,12 +4,16 @@ const city = require('./city.routes');
 const profession = require('./profession.routes');
 const post = require('./post.routes');
 const image = require('./image.routes');
+const products = require('./products.routes');
+const category = require('./category.routes');
 
 module.exports = [
   ['/', health],
   ['/user', user],
+  ['/products', products],
   ['/post', post],
   ['/city', city],
   ['/profession', profession],
   ['/image', image],
+  ['/category', category],
 ];
